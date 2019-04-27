1ed4ad1e2a8b8b2f1fd7c054a7b69cbcbcb4b6cb
Ravikumar M <rmittapa@uncc.edu>
2019-04-27 06:26:54 -0400

    Modifeid value

--- 8.assignment-mpi-pt-to-pt/8.assignment-mpi-pt-to-pt/pingpong/mpi_ping_pong.cpp
+++ 8.assignment-mpi-pt-to-pt/8.assignment-mpi-pt-to-pt/pingpong/mpi_ping_pong.cpp
@@ -8,39 +8,25 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: mpirun "<<argv[0]<<" <element>"<<std::endl;
     return -1;
   }
-  int element;
+  int element,value,mpi_rank, mpi_size;
   MPI_Init(&argc,&argv);
   element = stoi(argv[1]);
-
-  // rank and size
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  int number;
-  if (rank == 0) {
-      //element
-    number = element;
-      //send and receive
-    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    cout<<"Sending from 0 to 1"<<endl;
-    MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-    cout<<"Received after adding"<<endl;
-    cout<<"After adding 2: " <<number<<endl;
-  } else if (rank == 1) {
-      
-      //receive add and send
-    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-    cout<<"Received to 1 from 0"<<endl;
-
-    number = number + 2;
-    MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+  if (mpi_rank == 0) {
+    value = element;
+    MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    cout<<"0 Send to 1"<<endl;
+    MPI_Recv(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    cout<<"Recive After addind"<<endl;
+    cout<<"Final Value: " <<value<<endl;
+  } else if (mpi_rank == 1) {
+    MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    cout<<"Received by 1 from 0"<<endl;
+    value = value + 2;//add 2
+    MPI_Send(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     cout<<"Sending from 1 to 0 after adding 2"<<endl;
-
   }
-
-
   MPI_Finalize();
   return 0;
 }
